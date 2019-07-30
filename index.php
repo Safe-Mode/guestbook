459540c50cd3bf130ece03ecbc487d8c148f26d9
Safe_Mode <safe2mode@gmail.com>
2019-07-30 23:10:38 +0300

    Include bootstrap styles

--- index.php
+++ index.php
@@ -11,40 +11,62 @@ require_once('guestbook.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Guestbook</title>
+  <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous">
 </head>
 
 <body>
-  <form action="" method="POST">
-    <p>
-      <input type="text" name="name" placeholder="Имя">
-    </p>
+  <div class="container py-4">
+    <h1 class="mb-5">Гостевая книга</h1>
 
-    <p>
-      <textarea name="text" cols="30" rows="10" placeholder="Сообщение" required minlength="2"></textarea>
-    </p>
+    <form
+        class="mx-auto mb-5"
+        action=""
+        method="POST"
+        style="max-width: 600px;">
+      <p>
+        <input
+            class="form-control"
+            type="text"
+            name="name"
+            placeholder="Имя">
+      </p>
 
-    <p>
-      <button type="submit">Отправить</button>
-    </p>
-  </form>
+      <p>
+        <textarea
+            class="form-control"
+            name="text"
+            rows="7"
+            placeholder="Сообщение"
+            required
+            minlength="2"></textarea>
+      </p>
 
-  <br>
-  <br>
+      <p>
+        <button
+            class="btn btn-primary"
+            type="submit">Отправить</button>
+      </p>
+    </form>
 
-  <section class="guestbook">
-    <ul>
-      <?php foreach ($messages as $message) : ?>
-        <li>
-          <p>
-            <small><?= $message['date'] ?></small><br>
-            <b><?= $message['name'] ?></b>
-          </p>
+    <section class="guestbook">
+      <ul class="list-group">
+        <?php foreach ($messages as $message) : ?>
+          <li class="list-group-item">
+            <p>
+              <small><?= $message['date'] ?></small><br>
+              <b><?= $message['name'] ?></b>
+            </p>
 
-          <p><?= $message['text'] ?></p>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </section>
+            <p><?= $message['text'] ?></p>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </section>
+  </div>
 </body>
 
 </html>
