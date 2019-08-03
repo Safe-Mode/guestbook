82873f7b0293df107c9c5fe127a50c3228e4a4d1
Safe_Mode <safe2mode@gmail.com>
2019-08-04 00:43:46 +0300

    Fix messages output issue

--- pagination.php
+++ pagination.php
@@ -8,4 +8,4 @@ $page_quantity = (int) ceil($msg_quantity / MSG_PER_PAGE);
 $page_num = ($is_page_set) ? (int) $_GET['page'] : 1;
 $is_first = $page_num === 1;
 $is_last = $page_num === $page_quantity;
-$row_start = ($is_page_set) ? ($page_num - 1) * MSG_PER_PAGE + 1 : 1;
+$row_start = ($is_page_set) ? ($page_num - 1) * MSG_PER_PAGE : 0;
